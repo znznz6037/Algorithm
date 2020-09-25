ab58cc137deefedb8cf3e5b9d38c38717a6bc0e0
SeBeom Park <znznz6037@gmail.com>
2020-09-25 18:23:29 +0900

    10870번: 피보나치 수 5

---
+++ 10870.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int Fibo(int n) {
+	if (n <= 1) return n;
+	return Fibo(n - 1) + Fibo(n - 2);
+}
+
+int main() {
+	int n;
+	cin >> n;
+	cout << Fibo(n);
+	return 0;
+}
