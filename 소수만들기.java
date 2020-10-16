266678691fe514359fc9b585b97a63ea03ad60d2
SeBeom Park <znznz6037@gmail.com>
2020-10-16 15:02:45 +0900

    소수 만들기

---
+++ 소수만들기.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int solution(int[] nums) {
+        int answer = 0;
+
+        for(int i = 0; i < nums.length - 2; i++){
+            for(int j = i + 1; j < nums.length - 1; j++){
+                for(int k = j + 1; k < nums.length; k++){
+                    if(isPrime(nums[i] + nums[j] + nums[k])) {
+                        answer++;
+                    }
+                }
+            }
+        }
+        return answer;
+    }
+    
+    static boolean isPrime(int n){
+        for(int i = 2; i <= n / 2; i++){
+            if(n % i == 0) return false;
+        }
+        return true;
+    }
+}
