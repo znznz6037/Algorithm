34cd1e7f448d34fdb08a671926e68f1446e17bac
ParkSeBeom <znznz6037@gmail.com>
2021-09-19 18:28:58 +0900

    프로그래머스 위클리 챌린지 1주차_부족한 금액 계산하기
    소스코드 추가

--- CalcLackMoney.java
+++ CalcLackMoney.java
@@ -1,2 +1,7 @@
-public class CalcLackMoney {
+class Solution {
+    public long solution(int price, int money, int count) {
+        long answer = -1;
+        answer = (long)price * (count * (count + 1) / 2) - money;
+        return answer < 0 ? 0 : answer;
+    }
 }
