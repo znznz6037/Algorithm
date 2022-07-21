86995773dcf8af942560f784626f2afb1d25d925
ParkSeBeom <znznz6037@gmail.com>
2022-07-21 21:40:13 +0900

    9251번: LCS
    백준 골드 5
    DP
    https://www.acmicpc.net/problem/9251

---
+++ LCS.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class LCS {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str1 = sc.next();
+        String str2 = sc.next();
+        sc.close();
+
+        System.out.print(lcsLength(str1, str2));
+    }
+
+    public static int lcsLength(String str1, String str2) {
+        int[][] dp = new int[str1.length() + 1][str2.length() + 1];
+
+        for(int i = 1; i <= str1.length(); i++) {
+            for(int j = 1; j <= str2.length(); j++) {
+                if(str1.charAt(i - 1) == str2.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                }
+                else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+
+        return dp[str1.length()][str2.length()];
+    }
+}
