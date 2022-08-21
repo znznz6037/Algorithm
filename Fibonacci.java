877a023e06c25b3dc62f37563d7ab2ac0bd0ca36
ParkSeBeom <znznz6037@gmail.com>
2022-08-21 18:29:04 +0900

    피보나치 수
    프로그래머스 레벨2
    https://school.programmers.co.kr/learn/courses/30/lessons/12945

---
+++ Fibonacci.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int solution(int n) {
+        int[] fibo = new int[n + 1];
+        fibo[1] = fibo[2] = 1;
+
+        for(int i = 3; i <= n; i++) {
+            fibo[i] = (fibo[i - 1] + fibo[i - 2]) % 1234567;
+        }
+
+        return fibo[n];
+    }
+}
