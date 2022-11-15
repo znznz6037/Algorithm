af5d0eb4898a19e1f1ff71b426153bee31d37297
ParkSeBeom <znznz6037@gmail.com>
2022-11-15 19:58:34 +0900

    백준 실버3 1904번: 01타일
    DP
    https://www.acmicpc.net/problem/1904

---
+++ 1904.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[] dp = new int[N + 1];
+        br.close();
+
+        if(N < 3) {
+            System.out.println(N);
+            return;
+        }
+
+        for(int i = 2; i <= N - 2; i++) {
+            dp[i] = (dp[i - 1] + dp[i - 2]) % 15746;
+        }
+
+        System.out.println(dp[N]);
+    }
+}
